2c5cba055a3e8e997a36c4a115fe36b70a6e9cb0
JustTom77 <20codetom20@gmail.com>
2021-10-04 15:58:37 +0200

    fixed hamburger-menu bug

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 const navBar = document.getElementById("nav-bar");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
+if (screen.width >= 376) {
+  hamburgerMenu.style.display = "none";
+}
+
 window.addEventListener("click", e => {
-  if (e.target.className === "hamburger-menu") {
-    hamburgerMenu.style.display = "none";
-    navBar.style.display = "flex";
-  } else {
-    hamburgerMenu.style.display = "flex";
-    navBar.style.display = "none";
+  if (screen.width === 375) {
+    if (e.target.className === "hamburger-menu") {
+      hamburgerMenu.style.display = "none";
+      navBar.style.display = "flex";
+    } else {
+      hamburgerMenu.style.display = "flex";
+      navBar.style.display = "none";
+    }
   }
 });
 
 window.addEventListener("keydown", e => {
-  const backButton = e.keyCode;
-  if (backButton == 27) {
-    if (
-      (hamburgerMenu.style.display = "none" && (navBar.style.display = "flex"))
-    ) {
-      hamburgerMenu.style.display = "flex";
-      navBar.style.display = "none";
+  if (screen.width === 375) {
+    const backButton = e.keyCode;
+    if (backButton == 27) {
+      if (
+        (hamburgerMenu.style.display =
+          "none" && (navBar.style.display = "flex"))
+      ) {
+        hamburgerMenu.style.display = "flex";
+        navBar.style.display = "none";
+      }
     }
   }
 });
